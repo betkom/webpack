6a01e3cb2553b0c4d33d757c50af2bf12140e8a0
Zubin Talib <zubintalib@izooky.com>
2020-05-07 16:26:24 -0500

    Update webpack.config.prod.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,7 +3,6 @@ var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
 var commonConfig = require('./webpack.config.common.js');
 
-
 module.exports = webpackMerge(commonConfig,{
   mode: 'production',
   entry: {
